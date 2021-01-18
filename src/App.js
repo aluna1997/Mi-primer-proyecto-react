1b56f9b9369710e1204299106096bc9f227ceda8
aluna <aluna1997@ciencias.unam.mx>
2021-01-18 11:25:37 -0600

    Practica lista sin punto extra

--- src/App.js
+++ src/App.js
@@ -2,60 +2,25 @@ import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
-import { todos } from './todos.json'
+import Cards from './components/Cards';
 
 class App extends Component {
   constructor(){
     super();
     this.state={
       title:"To Do",
-      ntareas:10,
-      todos 
+      ntareas:10
     }
   }
 
-  removeTodo(index) {
-    console.log(index);
-    //if(window.confirm('¿Seguro de que quieres eliminar la tarea?')){
-      this.setState({
-        todos:this.state.todos.filter((todo,i) => {
-          return i != index
-        })
-      })
-    //}
-  }
-
-
   render(){
-    const todos = this.state.todos.map((todo,i) => {
-      return(
-        <div className="col-md-4">
-          <div className="card mt-4">
-              <div className="card-header">
-                  <h3 className="text-dark">{todo.title}</h3>
-                  <span className="badge badge-pill badge-danger ml-2 text-dark">{todo.priority}</span>
-              </div>
-              <div className="card-body">
-                <p><mark>{todo.description}</mark></p>
-                <p><mark>{todo.responsable}</mark></p>
-              </div>
-              <div className="card-footer">
-                <a onClick={this.removeTodo.bind(this,i)} href="#" role="button"> {this.props.title} &#x23F0;</a>
-              </div>
-          </div>
-        </div>
-      )
-    })
- 
-
-
     return (
       <div className="App">
         <header className="App-header">
-          <Navigation title={this.state.title} ntareas={todos.length}/>
+          <Navigation title={this.state.title} ntareas="5"/>
           <div className="container">
               <div className="row mt-4">
-                  {todos}
+                <Cards/>
               </div>
           </div>
           <img src={logo} className="App-logo" alt="logo" />
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,67 @@
 import React, { Component } from 'react';
+import { todos } from '../todos.json'
 
 class Cards extends Component{
-    render(){
-        return(
-        <nav className="navbar navbar-dark dg-dark">
-            <a href="" className="text-white"> <p>{this.props.title} &#x23F0;</p> </a>
-            <span className="badge badge-pill badge-light ml-2">{this.props.ntareas}</span>
-        </nav>
-      );
+    constructor(){
+        super();
+        this.state={
+          todos 
+        }
     }
+    
+    removeTodo(index) {
+        this.setState({
+        todos:this.state.todos.filter((todo,i) => {
+            return i != index
+        })
+        })
+    }
+
+    
+
+
+    render(){
+        const todos = this.state.todos.map((todo,i) => {
+        return(
+            <div className="col-md-4">
+              <div className="card mt-4">
+                  <div className="card-header">
+                      <h3 className="text-dark">{todo.title}</h3>
+                      <span className="badge badge-pill badge-danger ml-2 text-dark">{todo.priority}</span>
+                  </div>
+                  <div className="card-body">
+                    <p><mark>{todo.description}</mark></p>
+                    <p className="text-dark">{todo.responsable}</p>
+                  </div>
+                  <div className="card-footer">
+                  <a onClick={this.removeTodo.bind(this,i)} href="#" role="button"> {this.props.title} &#10004;</a>
+                  </div>
+              </div>
+            </div>
+        )
+      })
+
+      todos.push(
+        <div className="col-md-4">
+          <div className="card mt-4">
+              <div className="card-header">
+                  <h3 className="text-dark">Nueva tarea</h3>
+              </div>
+              <div className="card-body">
+                <p><mark>Holi</mark></p>
+                <a href="#" role="button"> {this.props.title} &#10004;</a>
+              </div>
+          </div>
+        </div>
+      )
+
+      return todos
+
+
+     
+    
+    }
+
 }
 
-export default Navigation;
+export default Cards;
